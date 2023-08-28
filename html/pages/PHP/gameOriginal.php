ba5738fd0abbdfad071ea04e0bb9030803fab2b8
Xenock20 <juliosaboredo@gmail.com>
2023-08-28 00:47:12 -0300

    Fix page index with style and funcionality

--- html/pages/PHP/gameOriginal.php
+++ html/pages/PHP/gameOriginal.php
@@ -1,14 +1,16 @@
 <?php
-$host = 'mysql';  // Nombre del servicio MySQL en Docker Compose
-$user = 'root';
-$password = 'rootadmin';
-$database = 'juego';
-
-$mysqli = new mysqli($host, $user, $password, $database);
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-
-echo "Connected successfully to MySQL!";
+    $name = $_GET['player']
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juego Original</title>
+    <link rel="stylesheet" href="styleGameOriginal.css">
+</head>
+<body>
+    <h1>Hola <?php echo $name?></h1>
+</body>
+</html>
